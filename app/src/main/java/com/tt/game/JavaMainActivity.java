7dbef7021181e656b188f95cdaa5223a0292b9ae
threedeer <1224179883@qq.com>
2020-04-15 12:12:20 +0800

    第一次提交

---
+++ app/src/main/java/com/tt/game/JavaMainActivity.java
@@ -0,0 +1,20 @@
+package com.tt.game;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * @author T
+ * @date 2020/4/14
+ */
+public class JavaMainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
