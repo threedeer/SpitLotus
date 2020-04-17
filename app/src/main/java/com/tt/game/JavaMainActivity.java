e041bc9ff426a3361c083084b3e6a906e0c7df21
hujintao <hujintao@fangtian.me>
2020-04-17 09:16:49 +0800

    第一次提交

--- app/src/main/java/com/tt/game/JavaMainActivity.java
+++ app/src/main/java/com/tt/game/JavaMainActivity.java
@@ -14,6 +14,7 @@ public class JavaMainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
 
